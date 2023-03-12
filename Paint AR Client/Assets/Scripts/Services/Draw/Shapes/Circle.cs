ec1edd38d79ddf67ec873d4c3b1a22a068d79c36
Makar Dezhemesov <dezhemesov.work@gmail.com>
2023-03-12 16:59:52 +0300

    Refactor: circle now uses local space

--- Paint AR Client/Assets/Scripts/Services/Draw/Shapes/Circle.cs
+++ Paint AR Client/Assets/Scripts/Services/Draw/Shapes/Circle.cs
@@ -5,34 +5,28 @@ namespace ArPaint.Services.Draw.Shapes
 {
     public class Circle : IShape
     {
-        private readonly Dictionary<IShapeContainer, Vector3> _startPositions = new();
-
         public void OnDrawStart(IShapeContainer container, Vector3 position)
         {
             container.IsLooping = true;
-            _startPositions.Add(container, position);
         }
 
         public void OnDrawMove(IShapeContainer container, Vector3 position)
         {
-            if (!_startPositions.TryGetValue(container, out var startPosition)) return;
-            
             container.Clear();
-            DrawCircle(container, startPosition, position);
+            DrawCircle(container, position);
         }
 
         public void OnDrawEnd(IShapeContainer container, Vector3 position)
         {
-            _startPositions.Remove(container);
         }
 
-        private void DrawCircle(IShapeContainer container, Vector3 startPosition, Vector3 endPosition)
+        private void DrawCircle(IShapeContainer container, Vector3 position)
         {
-            var radius = Vector3.Distance(startPosition, endPosition);
+            var radius = Vector3.Distance(Vector3.zero, position);
             for (var i = 0f; i < 360f; i++)
             {
                 var angle = i * Mathf.Deg2Rad;
-                var pointPos = startPosition + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
+                var pointPos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
                 container.AppendPosition(pointPos);
             }
         }
