6c7912439e26051b4b53f4dece311b3a50391735
Makar Dezhemesov <dezhemesov.work@gmail.com>
2023-03-12 17:04:59 +0300

    Refactor: oval now uses local space

--- Paint AR Client/Assets/Scripts/Services/Draw/Shapes/Oval.cs
+++ Paint AR Client/Assets/Scripts/Services/Draw/Shapes/Oval.cs
@@ -5,35 +5,27 @@ namespace ArPaint.Services.Draw.Shapes
 {
     public class Oval : IShape
     {
-        private readonly Dictionary<IShapeContainer, Vector3> _startPositions = new();
-
         public void OnDrawStart(IShapeContainer container, Vector3 position)
         {
             container.IsLooping = true;
-            _startPositions.Add(container, position);
         }
 
         public void OnDrawMove(IShapeContainer container, Vector3 position)
         {
-            if (!_startPositions.TryGetValue(container, out var startPosition)) return;
-            
             container.Clear();
-            DrawCircle(container, startPosition, position);
+            DrawCircle(container, position);
         }
 
         public void OnDrawEnd(IShapeContainer container, Vector3 position)
         {
-            _startPositions.Remove(container);
         }
 
-        private void DrawCircle(IShapeContainer container, Vector3 startPosition, Vector3 endPosition)
+        private void DrawCircle(IShapeContainer container, Vector3 position)
         {
-            var scale = endPosition - startPosition;
-            
             for (var i = 0f; i < 360f; i++)
             {
                 var angle = i * Mathf.Deg2Rad;
-                var pointPos = startPosition + new Vector3(Mathf.Cos(angle) * scale.x, Mathf.Sin(angle) * scale.y);
+                var pointPos = new Vector3(Mathf.Cos(angle) * position.x, Mathf.Sin(angle) * position.y);
                 container.AppendPosition(pointPos);
             }
         }
