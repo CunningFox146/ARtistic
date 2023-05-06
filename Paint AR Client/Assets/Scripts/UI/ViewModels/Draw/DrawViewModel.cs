18d432eb37e65ca8727eaae42f3621a0627430d1
Makar Dezhemesov <dezhemesov.work@gmail.com>
2023-05-07 01:37:08 +0300

    Feat: add undo/redo commands

--- Paint AR Client/Assets/Scripts/UI/ViewModels/Draw/DrawViewModel.cs
+++ Paint AR Client/Assets/Scripts/UI/ViewModels/Draw/DrawViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using ArPaint.Services.Commands;
 using ArPaint.Services.Draw;
 using ArPaint.Services.Draw.Shapes;
 using Services.StaticData;
 using UnityMvvmToolkit.Core;
 using UnityMvvmToolkit.Core.Attributes;
 using UnityMvvmToolkit.Core.Interfaces;
+using Command = UnityMvvmToolkit.Core.Command;
+using ICommand = UnityMvvmToolkit.Core.Interfaces.ICommand;
 
 namespace ArPaint.UI.ViewModels.Draw
 {
     public class DrawViewModel : ViewModel
     {
+        private readonly ICommandBuffer _commandBuffer;
         private readonly DrawService _drawService;
         
         [Observable(nameof(Shapes))]
@@ -20,6 +24,8 @@ namespace ArPaint.UI.ViewModels.Draw
         private readonly IProperty<bool> _isShapeSelectVisible;
 
         public ICommand ToggleShapeSelectCommand { get; }
+        public ICommand UndoCommand { get; }
+        public ICommand RedoCommand { get; }
         public ObservableCollection<ShapeViewModel> Shapes => _shapes.Value;
 
         public bool IsShapeSelectVisible
@@ -28,26 +34,39 @@ namespace ArPaint.UI.ViewModels.Draw
             set => _isShapeSelectVisible.Value = value;
         }
 
-        public DrawViewModel(IStaticDataService staticData, DrawService drawService)
+        public DrawViewModel(IStaticDataService staticData, ICommandBuffer commandBuffer, DrawService drawService)
         {
+            _commandBuffer = commandBuffer;
             _drawService = drawService;
 
             ToggleShapeSelectCommand = new Command(ToggleShapeSelect);
+            UndoCommand = new Command(Undo);
+            RedoCommand = new Command(Redo);
             _isShapeSelectVisible = new Property<bool>(false);
             _shapes = new ReadOnlyProperty<ObservableCollection<ShapeViewModel>>(new());
             
             InitShapes(staticData.Shapes.ShapesList);
         }
 
-        private void ToggleShapeSelect()
-        {
-            IsShapeSelectVisible = !IsShapeSelectVisible;
-        }
-
         private void InitShapes(List<Shape> shapes)
         {
             foreach (var shape in shapes)
                 Shapes.Add(new ShapeViewModel(shape, OnShapeSelected));
+        }
+
+        private void Undo()
+        {
+            _commandBuffer.UndoLastCommand();
+        }
+
+        private void Redo()
+        {
+            _commandBuffer.RedoLastCommand();
+        }
+
+        private void ToggleShapeSelect()
+        {
+            IsShapeSelectVisible = !IsShapeSelectVisible;
         }
 
         private void OnShapeSelected(Shape shape)
