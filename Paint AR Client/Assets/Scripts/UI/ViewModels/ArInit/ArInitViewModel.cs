404b88c619cba20bac3eb6a203ee98352d714084
Makar Dezhemesov <dezhemesov.work@gmail.com>
2023-05-27 14:02:27 +0300

    Feat: add back button to ArInit

--- Paint AR Client/Assets/Scripts/UI/ViewModels/ArInit/ArInitViewModel.cs
+++ Paint AR Client/Assets/Scripts/UI/ViewModels/ArInit/ArInitViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace ArPaint.UI.ViewModels.ArInit
+﻿using System.Threading;
+using ArPaint.Infrastructure.SceneManagement;
+using Cysharp.Threading.Tasks;
+using UnityMvvmToolkit.UniTask;
+using UnityMvvmToolkit.UniTask.Interfaces;
+
+namespace ArPaint.UI.ViewModels.ArInit
 {
     public class ArInitViewModel : ViewModel
     {
-        
+        private readonly ISceneLoader _sceneLoader;
+        public IAsyncCommand CloseViewCommand { get; }
+
+        public ArInitViewModel(ISceneLoader sceneLoader)
+        {
+            _sceneLoader = sceneLoader;
+            CloseViewCommand = new AsyncCommand(CloseView) { DisableOnExecution = true };
+        }
+
+        private async UniTask CloseView(CancellationToken _)
+        {
+            await _sceneLoader.LoadScene(SceneIndex.MainMenu);
+        }
     }
 }
